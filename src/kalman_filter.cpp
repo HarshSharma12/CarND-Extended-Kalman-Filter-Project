44c97788a80a0fd45f1c24df5069a8f7d6c992d2
Harsh Sharma <harsh.sharma@sevenlakes.com>
2017-07-11 23:27:46 +0545

    Upload to github

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_ ;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,9 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  // Kalman filter update step. Equations from the lectures
+  VectorXd y = z - (H_ * x_); // error calculation
+  KF(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +42,27 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  double theta = atan2(x_(1), x_(0));
+  double rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
+
+  VectorXd h = VectorXd(3);
+  h << rho, theta, rho_dot;
+
+  VectorXd y = z - h;
+  // normalise phi
+  y(1) = atan2(sin(y(1)), cos(y(1)));
+  KF(y);
+}
+
+// Universal update Kalman Filter step. Equations from the lectures
+void KalmanFilter::KF(const VectorXd &y){
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  // estimate
+  x_ = x_ + (K * y);
+  P_ -= K * H_ * P_;
 }
